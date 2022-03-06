7ebab701af3da31e0d4139b00d449ba50cebd73f
Mostafa Maher <mostafa_khashaba30@outlook.com>
2022-03-07 00:52:53 +0200

    section Event Js Upload

--- scripts/main.js
+++ scripts/main.js
@@ -105,7 +105,7 @@ function Testimonials() {
 
 let fields_Parent = document.querySelectorAll("#skills .container .dataprog section");
 let fields = document.querySelectorAll("#skills .container .dataprog section i i");
-console.log();
+
 window.onscroll = () => {
     // Services Section
     let width = 0,
@@ -149,31 +149,31 @@ function Mdeley() {
     })
 }
 
+//// //// //// //////////////////////////////////////////////////////////////////////
+
+
 /// Start Event Section ///
-/**
- *   <section class="events-list">
-            <div>
-                <h3>Title</h3>
-                <section class="btns">
-                    <button class="show">Show</button>
-                    <button class="cancel">Cancel</button>
-                </section>
-            </div>
-        </section>
- * 
- */
-let BtnShowEvent = document.querySelector("#events .Btn-event"),
+class Events { // Class Medul
+    constructor(Id, email, Title, Details) {
+        this.Id = Id;
+        this.Title = Title;
+        this.Details = Details;
+        this.email = email;
+        this.Done = false;
+    }
+}
+let BtnShowEvents = document.querySelector("#events .Btn-event"),
     EventsContainer = document.querySelector("#events .events-list"),
-    CloseEventsCont =document.querySelector("#events .events-list .CloseBtn") ;
-BtnShowEvent.onclick = () => {
-    EventsContainer.style.right = `0px`;
-    document.body.classList.toggle("over-lay");
-}
-CloseEventsCont.onclick= ()=>{
-    EventsContainer.style.right = `-500px`;
-    document.body.classList.toggle("over-lay");
-    
-}
+    EventsFields = document.querySelector("#events .events-list .events-fields"),
+    CloseEventsCont = document.querySelector("#events .events-list .CloseBtn");
+
+let ArrayOfEvents = []; /// Array Of Events
+
+let ExistenceEvent = [],
+    ThereEvent = false; // Events Available
+let EventNum = 0, // Current Event Number In ExistenceEvent
+    SubscribedEvent = false, // Check if current event is subscribed or not
+    MAX_NUM_OF_ELEMENT = 10; // Number Of Event Now
 
 let Date_ = { // object Has Four Date Elements
     Days: document.querySelector("#events .container .event .date .days p"),
@@ -181,4 +181,275 @@ let Date_ = { // object Has Four Date Elements
     Minutes: document.querySelector("#events .container .event .date .minutes p"),
     Seconds: document.querySelector("#events .container .event .date .seconds p"),
 };
-let subscribe = document.querySelector(`#events .subscribe input[type="submit"`); //subscribe Button
+
+let subscribe = document.querySelector(`#events .subscribe .BtnSubscribe`), //subscribe Button
+    otherEvent = document.querySelector(`#events .subscribe .otherEvent`); //Show Other Event Button
+let infoEvent = document.querySelector(`#events .container .event .info`)
+let CurrentDate_p = document.querySelector(".nearest-Event"), // Date Of Event
+    ResetSubscribedEvents = document.querySelector(".clear"); // 
+
+for (let i = 0; i < MAX_NUM_OF_ELEMENT; i++) { //Add Events In Available Events Array
+    let email = `mostafa.khashaba${i}0@gmail.com`,
+        title = `Title${`${i}`.repeat(3)}`,
+        details = `hello${i} `.repeat(10),
+        Event_Date = new Date(`202${i}`, `${i}`, `${i * 2}`).getTime();
+
+    let Event = new Events(Event_Date, email, title, details);
+
+    ExistenceEvent.push(Event);
+}
+let IntervalExist = false; // Determined Existence Of Interval
+let interval = 0; // Store Interval Id
+
+
+/*Start of Web Will Check If User Has Events Subscribed In His Local Storage
+Or No .
+If he Has We Will Put First Didn't End Event To Page .
+If He Hasn't We Will Put First Event Available From ExistenceEvent */
+
+SetTimeP(Date.now()); // Add Time Of Current Date
+
+StartWebSite(); //Call Function
+
+// Button On Click events
+
+subscribe.onclick = () => {
+    if (!SubscribedEvent) {
+        ArrayOfEvents.push(ExistenceEvent[EventNum - 1]); // set Event Object Direct From Available Events
+        // EventNum - 1 Becuase We Increment it When Show Them 
+
+        AddToLocalStorage(ArrayOfEvents);
+
+        ExistenceEvent = ExistenceEvent.filter((el, index) => {
+            return index !== (EventNum - 1);
+        });
+
+        MAX_NUM_OF_ELEMENT--;
+        EventNum--;
+        SubscribedEvent = true; // Current Event Has Been Subscribed
+        // We Want Now To Remove That Event From Available
+    }
+    // console.log(`event num ${EventNum}`);//Test
+    // console.log(`Max event num ${MAX_NUM_OF_ELEMENT}`);//Test
+}
+
+BtnShowEvents.onclick = () => {
+    EventsContainer.style.right = `0px`;
+    document.body.classList.toggle("over-lay");
+}
+
+CloseEventsCont.onclick = () => {
+    EventsContainer.style.right = `-500px`;
+    document.body.classList.toggle("over-lay");
+
+}
+
+ResetSubscribedEvents.onclick = () => {
+    AddToLocalStorage([]); // Reset LocalStorage
+    StartWebSite(); // Called To Determined Add Event Or What
+    SubscribedEvent = false; // No Event Subscribed
+}
+
+/// /////// /////// ///////
+
+function StartWebSite() { // function Called When Web Site Open
+
+    GetFromLocalStorage(); // Get Events From Local Storage At Starts
+
+    if (ArrayOfEvents.length > 0) {
+        for (let i = 0; i < ArrayOfEvents.length; i++) {
+            if (!ArrayOfEvents[i].Done) {
+                SendEventToTimer(ArrayOfEvents[i]); // Add First Event Didn't End To Page
+                break;
+            } else {
+                if (i === ArrayOfEvents.length - 1) { // Thats Mean All Events End 
+                    AvailableEvents();
+                }
+            }
+        }
+    } else {
+        AvailableEvents();
+    }
+}
+
+function AvailableEvents() {
+    if (ExistenceEvent.length > 0) { // Add First Event From ExistenceEvent
+        SendEventToTimer(ExistenceEvent[0]);
+    } else { // If We Doesn't Have Events Available 
+        // We Will Show Message 
+        ResetDateFields();
+        infoEvent.firstElementChild.innerHTML = `You Haven't Available Events`;
+        infoEvent.lastElementChild.innerHTML = `We Are Sorry, You Haven't Available Events Right Now
+                                                You Can Come Later. Sorry Again`;
+    }
+}
+
+function SetTimeP(DateEvent) {
+    CurrentDate_p.innerHTML = new Date(DateEvent).toString().substring(0, 25);
+}
+
+function CreateEventHtml(DateId, Title, Done) {
+    let DivContent = document.createElement("div"), // Create Div To Catch Content
+        H3 = document.createElement("h3"), // Create h3 Element Title
+        ButtonParent = document.createElement("section"), // Section Buttons
+        ShowBtn = document.createElement("button"), //Button To Show Content Of Event
+        CancelBtn = document.createElement("button"); // Button To Cancel Event
+
+    H3.innerHTML = Title; //Assign Title To H3
+    ButtonParent.classList.add("btns"); //Add Class Name btns To Element
+    ShowBtn.classList.add("Btn-show"); // Add Class To Btn
+    CancelBtn.classList.add("Btn-cancel"); // Add Class To Btn
+    ShowBtn.innerHTML = 'Show'; // Add text show to btn
+    CancelBtn.innerHTML = 'Cancel'; // add text cancel to btn
+
+    ButtonParent.appendChild(ShowBtn); // Append Btn Show 
+    ButtonParent.appendChild(CancelBtn); // Append Btn Cancel
+
+    DivContent.appendChild(H3); // Append H3 Title To Content
+    DivContent.appendChild(ButtonParent); // Append Buttons To Content
+
+    DivContent.setAttribute("data-EventId", `${DateId}`); // set Id To Element
+    DivContent.classList.add("event")
+    if (Done) {
+        DivContent.classList.add("Done");
+    }
+    return DivContent;
+}
+
+function TimerChangeDate(event, Interval) {
+
+    let Now_ = Date.now();
+
+    let DiffDate = event.Id - Now_; //Difference Between Two Date
+
+    let DAYs = Math.floor((DiffDate / (1000 * 60 * 60 * 24))), //Calc Number Of Days 
+
+        HOURs = Math.floor((DiffDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)), // Calc Number Of Hours 
+        // Get Remainder Number From Days Thats Will Be Number Of hours In MSeconds
+
+        MINUTEs = Math.floor((DiffDate % (1000 * 60 * 60)) / (1000 * 60)), //Calc Number Of Minutes
+        //Get Remainder Number From Hours Thats Will Be Number Of Minutes In MSeconds              
+
+        SECONDs = Math.floor((DiffDate % (1000 * 60)) / (1000)); // Calc Number Of Seconds
+    // Get Remainder Number From Minutes Thats Will Be Number Of Seconds In MSeconds 
+
+    // console.log(DAYs); // Test
+    // console.log(HOURs);// Test
+    // console.log(MINUTEs);//Test
+    // console.log(SECONDs);//Text
+
+    Date_.Days.innerHTML = DAYs >= 10 ? DAYs : `0${DAYs}`;
+
+    Date_.Hours.innerHTML = HOURs >= 10 ? HOURs : `0${HOURs}`;
+
+    Date_.Minutes.innerHTML = MINUTEs >= 10 ? MINUTEs : `0${MINUTEs}`;
+
+    Date_.Seconds.innerHTML = SECONDs >= 10 ? SECONDs : `0${SECONDs}`;
+
+    if (DiffDate <= 1) {
+        ResetDateFields();
+
+        event.Done = true; // Event End
+
+        for (const el in Date_) { // Add Class Flash To Box Date
+            Date_[el].parentElement.classList.toggle("flash");
+        }
+    } // if Event Date Come The Timer End
+
+}
+
+function TimerDate(event) {
+    let Interval = setInterval(() => {
+        TimerChangeDate(event, Interval);
+    }, 1000);
+
+    return Interval;
+}
+
+function EventToField(ArrayOfEvents) {
+
+    EventsFields.innerHTML = ''; //Reset Container Html
+    let EventField;
+    ArrayOfEvents.forEach((el) => {
+        EventField = CreateEventHtml(el.Id, el.Title, el.Done);
+        EventsFields.appendChild(EventField);
+    });
+
+    // let arr = document.querySelectorAll("#events .events-list .events-fields div");
+    let arr = EventsFields.childNodes;
+
+
+    arr.forEach((eventField, index) => { // Set Events On Buttons And Element onClick
+        eventField.onclick = () => {
+            let DateEvent_Id = eventField.getAttribute("data-EventId");
+            console.log(+DateEvent_Id)
+            SetTimeP(+DateEvent_Id); // Put Event Date To Current Event Clicked To P
+        };
+
+        eventField.lastElementChild.firstElementChild.onclick = () => {
+            SendEventToTimer(ArrayOfEvents[index]); // Btn Show Will Send Event To Show In Page
+            SubscribedEvent = true; // Event Already Subscribed 
+        };
+
+        eventField.lastElementChild.lastElementChild.onclick = () => {
+            ArrayOfEvents = ArrayOfEvents.filter((el, indexChild) => index !== indexChild); // Delete Event
+            AddToLocalStorage(ArrayOfEvents); // Will Delete From Local Storage
+        };
+    })
+}
+
+function AddToLocalStorage(ArrayOfEvents) {
+    window.localStorage.setItem('Events', JSON.stringify(ArrayOfEvents)); //Add To Local Storage
+    GetFromLocalStorage();
+}
+
+function GetFromLocalStorage() {
+    if (window.localStorage.getItem("Events")) {
+        ArrayOfEvents = JSON.parse(window.localStorage.getItem("Events"));
+        EventToField(ArrayOfEvents);
+        // TimerDate(ArrayOfEvents[5]);//Send First Event In Local Storage
+    }
+}
+
+otherEvent.onclick = () => {
+    if (MAX_NUM_OF_ELEMENT !== 0) {
+
+        if (EventNum === (MAX_NUM_OF_ELEMENT)) {
+            (EventNum = 0)
+        }
+
+        // if (SubscribedEvent) {
+        SubscribedEvent = false;
+        // } // Current Event Hasn't Been Subscribed
+        SendEventToTimer(ExistenceEvent[EventNum++]);
+
+        // console.log(`event num ${EventNum} in Other`);//Test
+        // console.log(`Max event num ${MAX_NUM_OF_ELEMENT} in other`);//Test
+    } else {
+        AvailableEvents();
+    }
+}
+
+function SendEventToTimer(Event) {
+    for (const el in Date_) { // Add Class Flash To Box Date
+        Date_[el].parentElement.classList.remove("flash");
+    }
+    if (ThereEvent) {
+        clearInterval(interval);
+        interval = TimerDate(Event);
+    } else {
+        ThereEvent = true;
+        interval = TimerDate(Event);
+    }
+
+    infoEvent.firstElementChild.innerHTML = Event.Title;
+    infoEvent.lastElementChild.innerHTML = Event.Details;
+}
+
+function ResetDateFields() { // reset field of Date Timer 
+    Date_.Days.innerHTML = '00';
+    Date_.Hours.innerHTML = '00';
+    Date_.Minutes.innerHTML = '00';
+    Date_.Seconds.innerHTML = '00';
+    clearInterval(interval);
+}
